bc0959808234fe094f491604072ec3bdfc136caa
mamadou.Dembele <mamadou.dembele@ensta-bretagne.org>
2021-02-17 10:13:23 +0100

    affichage pince

--- robot_tennis_gazebo/launch/gazebo.launch.py
+++ robot_tennis_gazebo/launch/gazebo.launch.py
@@ -11,12 +11,13 @@ def generate_launch_description():
     pkg_share1 = FindPackageShare('robot_tennis').find('robot_tennis')
     pkg_share2 = FindPackageShare('gazebo_ros').find('gazebo_ros')
     model_file = os.path.join(pkg_share1, 'urdf', "robot_tennis.urdf")
+    #mesh_file = os.path.join(pkg_share1, 'meshs', "pince2.dae")
     path = os.path.join(pkg_share2, 'launch', "gazebo.launch.py")
     #rviz_config_file = os.path.join(pkg_share, 'config', "display.rviz")
     
     robot_state_publisher_node=Node(
         package="robot_state_publisher", node_executable="robot_state_publisher",
-        arguments=[model_file]
+        arguments=[model_file]#, mesh_file]
         )
     rqt_robot_steering_node=Node(package="rqt_robot_steering", node_executable="rqt_robot_steering")
     gazebo = IncludeLaunchDescription(
